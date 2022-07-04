8cf0efe6c37e1cb807a9bac120a0f2d46f1eebc1
Amit Kumar <20bcs028@iiitdmj.ac.in>
2022-07-04 23:56:35 +0530

    Dynamic data work on rightbar

---
+++ client/src/components/onlineUsers/OnlineUsers.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function OnlineUsers(props) {
+  return (
+    <div className="online-users-container">
+        <div className="online-friends-list">
+            <div className="online-friend">
+              <img src="./assets/person/8.jpeg" alt="" className="friend-profile-pic" />
+              <span className="img-highlight-green"></span>
+              <span className="friend-name">Sansa Turner</span>
+            </div>
+        </div>
+    </div>
+  )
+}
