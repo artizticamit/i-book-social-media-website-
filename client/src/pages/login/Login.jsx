556a6fd8f5d61877b635a91dc7aa51e85bc18d79
Sunscar-Sony <166742286+Sunscar-Sony@users.noreply.github.com>
2024-10-21 16:00:59 +0530

    Login page UI enhanced and make it responsive, Updated the meta details for better SEO

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,148 +1,143 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CircularProgress from '@mui/material/CircularProgress';
-// import { Navigate } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { loginCall } from '../../apiCalls';
-import { AuthContext } from '../../context/AuthContext';
-import { useContext, useState } from 'react';
-import { useLogin } from '../../hooks/useLogin';
-// import {useRef} from 'react';
-// import { LoginContext } from '../contexts/LoginContext.jsx';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useContext, useState } from "react";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import "./Login.css";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://github.com/artizticamit/">
         Github
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const theme = createTheme();
 
-export default function Login(props) {
-  
-
-  // const email = useRef();
-  // const password = useRef();
-  // const {login, error, isLoading} = useLogin();
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  // const {loggedIn, setLoggedIn} = useContext(LoginContext);
-
-  const {user,isFetching, error, dispatch} = useContext(AuthContext)
-
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    loginCall({email:email, password:password},dispatch)
-    // login({email:email, password:password});
+    loginCall({ email, password }, dispatch);
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <div component="main" className="login-container">
         <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <Box component="form"  noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required={true}
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              value={email}
-              onChange={(e)=>{setEmail(e.target.value)}}
-              // ref={email}
-              autoFocus
+        <div className="login-wrapper">
+          <div className="login-left">
+            <Typography component="h1" variant="h4" className="login-heading">
+              Welcome Back!
+            </Typography>
+            <img
+              src="/assets/login-hero.png"
+              alt="Login Hero"
+              className="login-hero-image"
             />
-            <TextField
-              margin="normal"
-              required={true}
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              minLength="6"
-              autoComplete="current-password"
-              value={password}
-              onChange={(e)=>{setPassword(e.target.value)}}
-              // ref={password}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              disabled={isFetching}
-              onClick={handleSubmit}
-            >
-             {isFetching? <CircularProgress color="inherit" size="24px"/> : "Sign In"}
-            </Button>
-            {error&&<div className='error' style={{
-              border:'1px solid red',
-              borderRadius:'5px',
-              padding:'1px 5px 1px 5px',
-              color:'red',
-              backgroundColor:'#ffe0e0',
-              margin:'5px 0px 10px 0px'
-            }}>{error}</div>}
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
+          </div>
+
+          <div className="login-right">
+            <Box className="login-box">
+              <Avatar className="login-avatar">
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign In
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                className="login-form"
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  disabled={isFetching}
+                  className="login-button"
+                >
+                  {isFetching ? (
+                    <CircularProgress color="inherit" size="24px" />
+                  ) : (
+                    "Sign In"
+                  )}
+                </Button>
+                {error && <div className="error">{error}</div>}
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/signup" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+            <Copyright sx={{ mt: 8 }} />
+          </div>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
